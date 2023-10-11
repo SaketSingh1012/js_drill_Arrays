1ff3fa312150b2734dec9e90712b26cbec942df5
Saket Singh <81484363+SaketSingh1012@users.noreply.github.com>
2023-10-11 22:26:30 +0530

    completed Filter Function

---
+++ filter.js
@@ -0,0 +1,12 @@
+function filter(elements, cb) {
+    const result = [];
+    for (let i = 0; i < elements.length; i++) {
+      if (cb(elements[i])) {
+        result.push(elements[i]);
+      }
+    }
+    return result;
+  }
+  
+  module.exports = filter;
+  
